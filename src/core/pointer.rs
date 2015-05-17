63640bb6605955a66aea86f87667bdffc8e264df
Victor Berger <victor.berger@m4x.org>
2015-05-17 17:03:02 +0200

    Add "handled surface" notion to Pointer.
    And make the whole thing actually thread-safe.

--- src/core/pointer.rs
+++ src/core/pointer.rs
@@ -1,5 +1,9 @@
 use libc::c_void;
 
+use std::cell::Cell;
+use std::collections::HashSet;
+use std::sync::Mutex;
+
 use super::{From, Seat, Surface, SurfaceId, WSurface};
 use super::surface::wrap_surface_id;
 
@@ -29,7 +33,9 @@ pub fn wrap_pointer_id(p: usize) -> PointerId {
 struct PointerData<S: Surface> {
     pub surface: Option<S>,
     pub hidden: bool,
-    pub hotspot: (i32, i32)
+    pub hotspot: (i32, i32),
+    pub surfaces: HashSet<SurfaceId>,
+    pub is_on_handled_surface: Cell<bool>
 }
 
 impl<S: Surface> PointerData<S> {
@@ -37,21 +43,28 @@ impl<S: Surface> PointerData<S> {
         PointerData {
             surface: surface,
             hidden: false,
-            hotspot: hotspot
+            hotspot: hotspot,
+            surfaces: HashSet::new(),
+            is_on_handled_surface: Cell::new(false)
         }
     }
 
     fn unwrap_surface(&mut self) -> Option<S> {
-        ::std::mem::replace(&mut self.surface, None)
+        self.surface.take()
     }
 }
 
 trait CursorAdvertising {
-    unsafe fn advertise_cursor(&self, serial: u32, pointer: *mut wl_pointer);
+    unsafe fn advertise_cursor(&self, surface: SurfaceId, serial: u32, pointer: *mut wl_pointer);
+    fn enter_surface(&self, surface: SurfaceId) -> bool;
+    fn leave_surface(&self) -> bool;
+    fn is_on_handled_surface(&self) -> bool;
 }
 
 impl<S: Surface> CursorAdvertising for PointerData<S> {
-        unsafe fn advertise_cursor(&self, serial: u32, pointer: *mut wl_pointer) {
+    unsafe fn advertise_cursor(&self, surface: SurfaceId, serial: u32, pointer: *mut wl_pointer) {
+        // only advertize on surface we handle
+        if !self.surfaces.contains(&surface) { return; }
         if let Some(ref s) = self.surface {
             wl_pointer_set_cursor(
                 pointer,
@@ -64,12 +77,31 @@ impl<S: Surface> CursorAdvertising for PointerData<S> {
             wl_pointer_set_cursor(pointer, serial, ::std::ptr::null_mut(), 0, 0)
         }
     }
+
+    fn enter_surface(&self, surface: SurfaceId) -> bool {
+        if self.surfaces.contains(&surface) {
+            self.is_on_handled_surface.set(true);
+            true
+        } else {
+            false
+        }
+    }
+
+    fn leave_surface(&self) -> bool {
+        let was_on_surface = self.is_on_handled_surface.get();
+        self.is_on_handled_surface.set(false);
+        was_on_surface
+    }
+
+    fn is_on_handled_surface(&self) -> bool {
+        self.is_on_handled_surface.get()
+    }
 }
 
 
 
 struct PointerListener {
-    data: &'static CursorAdvertising,
+    data: Mutex<&'static CursorAdvertising>,
     enter_handler: Box<Fn(PointerId, SurfaceId, f64, f64) + 'static + Send + Sync>,
     leave_handler: Box<Fn(PointerId, SurfaceId) + 'static + Send + Sync>,
     motion_handler: Box<Fn(PointerId, u32, f64, f64) + 'static + Send + Sync>,
@@ -80,13 +112,48 @@ struct PointerListener {
 impl PointerListener {
     pub fn new(data: &'static CursorAdvertising) -> PointerListener {
         PointerListener {
-            data: data,
+            data: Mutex::new(data),
             enter_handler: Box::new(move |_,_,_,_| {}),
             leave_handler: Box::new(move |_,_| {}),
             motion_handler: Box::new(move |_,_,_,_| {}),
             button_handler: Box::new(move |_,_,_,_| {}),
             axis_handler: Box::new(move |_,_,_,_| {}),
         }
+    }
+
+    pub fn set_enter_handler<F>(&mut self, f: F)
+        where F: Fn(PointerId, SurfaceId, f64, f64) + 'static + Send + Sync
+    {
+        let _lock = self.data.lock().unwrap();
+        self.enter_handler = Box::new(f)
+    }
+
+    pub fn set_leave_handler<F>(&mut self, f: F)
+        where F: Fn(PointerId, SurfaceId) + 'static + Send + Sync
+    {
+        let _lock = self.data.lock().unwrap();
+        self.leave_handler = Box::new(f)
+    }
+
+    pub fn set_motion_handler<F>(&mut self, f: F)
+        where F: Fn(PointerId, u32, f64, f64) + 'static + Send + Sync
+    {
+        let _lock = self.data.lock().unwrap();
+        self.motion_handler = Box::new(f)
+    }
+
+    pub fn set_button_handler<F>(&mut self, f: F)
+        where F: Fn(PointerId, u32, u32, ButtonState) + 'static + Send + Sync
+    {
+        let _lock = self.data.lock().unwrap();
+        self.button_handler = Box::new(f)
+    }
+
+    pub fn set_axis_handler<F>(&mut self, f: F)
+        where F: Fn(PointerId, u32, ScrollAxis, f64) + 'static + Send + Sync
+    {
+        let _lock = self.data.lock().unwrap();
+        self.axis_handler = Box::new(f)
     }
 }
 
@@ -150,41 +217,80 @@ impl<S: Surface> Pointer<S> {
     /// the cursor surface in the `enter` event, which is processed before the
     /// surface is transmitted to the wayland server.
     ///
-    /// This method consumes the `Pointer`object to create a new one and releasing 
+    /// This method consumes the `Pointer` object to create a new one and releasing 
     /// the previous surface.
     ///
     /// If surface is `None`, the cursor display won't be changed when it enters a
     /// surface of this application.
+    ///
+    /// This will unhide the pointer if it was hidden.
+    ///
+    /// **Warning:** doing this from inside one of this pointer's event callabacks
+    /// **will** result in a deadlock. However, changing the buffer associated
+    /// with the cursor's surface is not a problem.
     pub fn set_cursor<NS: Surface>(mut self,
                                    surface: Option<NS>,
                                    hotspot: (i32, i32))
         -> (Pointer<NS>, Option<S>)
     {
         unsafe {
-            let old_surface = self.data.unwrap_surface();
             let new_data = Box::new(PointerData::new(surface, hotspot));
-            let mut listener = ::std::mem::replace(&mut self.listener, ::std::mem::uninitialized() );
-            listener.data = ::std::mem::transmute(&*new_data as &CursorAdvertising) ;
+            let listener = ::std::mem::replace(&mut self.listener, ::std::mem::uninitialized() );
+            {
+                let mut lock = listener.data.lock().unwrap();
+                *lock = ::std::mem::transmute(&*new_data as &CursorAdvertising);
+            }
             let new_pointer = Pointer {
                 seat: self.seat.clone(),
                 ptr: self.ptr,
                 listener: listener,
                 data: new_data
             };
+            let old_surface = self.data.unwrap_surface();
             ::std::mem::forget(self);
             (new_pointer, old_surface)
         }
     }
 
     /// Hides or unhides the cursor image.
+    ///
+    /// **Warning:** doing this from inside one of the event callabacks **will** result in
+    /// a deadlock.
     pub fn set_cursor_hidden(&mut self, hidden: bool) {
+        let _lock = self.listener.data.lock().unwrap();
         self.data.hidden = hidden
     }
 
-    pub fn release(self) {
+    /// Adds a surface to the set of surfaces this pointer handler
+    /// will change cursor and dispatch events.
+    ///
+    /// Does nothing if the surface is already handled by this pointer.
+    ///
+    /// **Warning:** doing this from inside one of this pointer's event callabacks
+    /// **will** result in a deadlock.
+    pub fn add_handled_surface(&mut self, surface: SurfaceId) {
+        let _lock = self.listener.data.lock().unwrap();
+        self.data.surfaces.insert(surface);
+    }
+
+    /// Adds a surface to the set of surfaces this pointer handler
+    /// will change cursor and dispatch events.
+    ///
+    /// Does nothing if the surface is already handled by this pointer.
+    ///
+    /// **Warning:** doing this from inside one of this pointer's event callabacks
+    /// **will** result in a deadlock.
+    pub fn remove_handled_surface(&mut self, surface: SurfaceId) {
+        let _lock = self.listener.data.lock().unwrap();
+        self.data.surfaces.remove(&surface);
+    }
+
+    pub fn release(mut self) -> Option<S> {
         unsafe {
             wl_pointer_release(self.ptr);
+            let surface = self.data.unwrap_surface();
             ::std::mem::forget(self);
+            surface
         }
     }
 
@@ -202,11 +308,14 @@ impl<S: Surface> Pointer<S> {
     ///
     /// - Id of the pointer
     /// - Id of the surface
-    /// - x and y, coordinates of the surface where the cursor entered
+    /// - x and y, coordinates of the surface where the cursor entered.
+    ///
+    /// **Warning:** doing this from inside one of this pointer's event callabacks
+    /// **will** result in a deadlock.
     pub fn set_enter_action<F>(&mut self, f: F)
         where F: Fn(PointerId, SurfaceId, f64, f64) + 'static + Send + Sync
     {
-        self.listener.enter_handler = Box::new(f)
+        self.listener.set_enter_handler(f);
     }
 
     /// Defines the action to be executed when the cursor leaves a surface.
@@ -218,10 +327,13 @@ impl<S: Surface> Pointer<S> {
     ///
     /// - Id of the pointer
     /// - Id of the surface
+    ///
+    /// **Warning:** doing this from inside one of this pointer's event callabacks
+    /// **will** result in a deadlock.
     pub fn set_leave_action<F>(&mut self, f: F)
         where F: Fn(PointerId, SurfaceId) + 'static + Send + Sync
     {
-        self.listener.leave_handler = Box::new(f)
+        self.listener.set_leave_handler(f);
     }
 
     /// Defines the action to be executed when the cursor moves
@@ -231,10 +343,13 @@ impl<S: Surface> Pointer<S> {
     /// - Id of the pointer
     /// - time of the event
     /// - x and y, new coordinates of the cursor relative to the current surface
+    ///
+    /// **Warning:** doing this from inside one of this pointer's event callabacks
+    /// **will** result in a deadlock.
     pub fn set_motion_action<F>(&mut self, f: F)
         where F: Fn(PointerId, u32, f64, f64) + 'static + Send + Sync
     {
-        self.listener.motion_handler = Box::new(f)
+        self.listener.set_motion_handler(f);
     }
 
     /// Defines the action to be executed when a button is clicked or released
@@ -245,10 +360,13 @@ impl<S: Surface> Pointer<S> {
     /// - time of the event
     /// - button of the event,
     /// - new state of the button
+    ///
+    /// **Warning:** doing this from inside one of this pointer's event callabacks
+    /// **will** result in a deadlock.
     pub fn set_button_action<F>(&mut self, f: F)
         where F: Fn(PointerId, u32, u32, ButtonState) + 'static + Send + Sync
     {
-        self.listener.button_handler = Box::new(f)
+        self.listener.set_button_handler(f);
     }
 
     /// Defines the action to be executed when a scrolling is done
@@ -259,10 +377,13 @@ impl<S: Surface> Pointer<S> {
     /// - time of the event
     /// - the axis that is scrolled
     /// - the amplitude of the scrolling
+    ///
+    /// **Warning:** doing this from inside one of this pointer's event callabacks
+    /// **will** result in a deadlock.
     pub fn set_axis_action<F>(&mut self, f: F)
         where F: Fn(PointerId, u32, ScrollAxis, f64) + 'static + Send + Sync
     {
-        self.listener.axis_handler = Box::new(f)
+        self.listener.set_axis_handler(f);
     }
 }
 
@@ -295,13 +416,19 @@ extern "C" fn pointer_enter_handler(data: *mut c_void,
                                     surface_y: i32
                                    ) {
     let listener = unsafe { &*(data as *const PointerListener) };
-    (*listener.enter_handler)(
-        wrap_pointer_id(pointer as usize),
-        wrap_surface_id(surface as usize),
-        wl_fixed_to_double(surface_x),
-        wl_fixed_to_double(surface_y)
-    );
-    unsafe { listener.data.advertise_cursor(serial, pointer) }
+    let lock = listener.data.lock().unwrap();
+    let sid = wrap_surface_id(surface as usize);
+    if lock.enter_surface(sid) {
+        (*listener.enter_handler)(
+            wrap_pointer_id(pointer as usize),
+            sid,
+            wl_fixed_to_double(surface_x),
+            wl_fixed_to_double(surface_y)
+        );
+        unsafe {
+            lock.advertise_cursor(sid, serial, pointer)
+        }
+    }
 }
 
 extern "C" fn pointer_leave_handler(data: *mut c_void,
@@ -310,10 +437,13 @@ extern "C" fn pointer_leave_handler(data: *mut c_void,
                                     surface: *mut wl_surface
                                    ) {
     let listener = unsafe { &*(data as *const PointerListener) };
-    (*listener.leave_handler)(
-        wrap_pointer_id(pointer as usize),
-        wrap_surface_id(surface as usize)
-    );
+    let lock = listener.data.lock().unwrap();
+    if lock.leave_surface() {
+        (*listener.leave_handler)(
+            wrap_pointer_id(pointer as usize),
+            wrap_surface_id(surface as usize)
+        );
+    }
 }
 
 extern "C" fn pointer_motion_handler(data: *mut c_void,
@@ -323,12 +453,15 @@ extern "C" fn pointer_motion_handler(data: *mut c_void,
                                      surface_y: i32
                                     ) {
     let listener = unsafe { &*(data as *const PointerListener) };
-    (*listener.motion_handler)(
-        wrap_pointer_id(pointer as usize),
-        time,
-        wl_fixed_to_double(surface_x),
-        wl_fixed_to_double(surface_y)
-    );
+    let lock = listener.data.lock().unwrap();
+    if lock.is_on_handled_surface() {
+        (*listener.motion_handler)(
+            wrap_pointer_id(pointer as usize),
+            time,
+            wl_fixed_to_double(surface_x),
+            wl_fixed_to_double(surface_y)
+        );
+    }
 }
 
 extern "C" fn pointer_button_handler(data: *mut c_void,
@@ -339,12 +472,15 @@ extern "C" fn pointer_button_handler(data: *mut c_void,
                                      state: ButtonState
                                     ) {
     let listener = unsafe { &*(data as *const PointerListener) };
-    (*listener.button_handler)(
-        wrap_pointer_id(pointer as usize),
-        time,
-        button,
-        state
-    );
+    let lock = listener.data.lock().unwrap();
+    if lock.is_on_handled_surface() {
+        (*listener.button_handler)(
+            wrap_pointer_id(pointer as usize),
+            time,
+            button,
+            state
+        );
+    }
 }
 
 extern "C" fn pointer_axis_handler(data: *mut c_void,
@@ -354,12 +490,15 @@ extern "C" fn pointer_axis_handler(data: *mut c_void,
                                    value: i32
                                   ) {
     let listener = unsafe { &*(data as *const PointerListener) };
-    (*listener.axis_handler)(
-        wrap_pointer_id(pointer as usize),
-        time,
-        axis,
-        wl_fixed_to_double(value)
-    );
+    let lock = listener.data.lock().unwrap();
+    if lock.is_on_handled_surface() {
+        (*listener.axis_handler)(
+            wrap_pointer_id(pointer as usize),
+            time,
+            axis,
+            wl_fixed_to_double(value)
+        );
+    }
 }
 
 static POINTER_LISTENER: wl_pointer_listener = wl_pointer_listener {
