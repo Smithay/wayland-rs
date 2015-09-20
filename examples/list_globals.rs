17889a6a075b06d1871828439d1405d2149cb56e
Victor Berger <victor.berger@m4x.org>
2015-09-20 10:30:37 +0200

    Document a little more the example.

--- examples/list_globals.rs
+++ examples/list_globals.rs
@@ -1,19 +1,35 @@
 extern crate wayland_client as wayland;
 
-use wayland::{Proxy, EventIterator};
+use wayland::{Proxy, EventIterator, Event};
+use wayland::wayland::{WaylandProtocolEvent, WlRegistryEvent};
 
 fn main() {
-    let mut display = wayland::wayland::get_display().unwrap();
+    let mut display = match wayland::wayland::get_display() {
+        Some(d) => d,
+        None => panic!("Unable to connect to a wayland compositor.")
+    };
 
+    // Create an event iterator and assign it to the display
+    // so that it is automatically inherited by all created objects
     let evt_iter = EventIterator::new();
     display.set_evt_iterator(&evt_iter);
 
-    let mut registry = display.get_registry();
-    registry.set_evt_iterator(&evt_iter);
+    // Get the registry, to generate the events advertizing global objects
+    let registry = display.get_registry();
 
+    // Roundtrip, to make sure all event are dispatched to us
     display.sync_roundtrip();
 
     for evt in evt_iter {
-        println!("{:?}", evt);
+        match evt {
+            // Global advertising events are `WlRegistryEvent::Global`
+            Event::Wayland(WaylandProtocolEvent::WlRegistry(
+                _, WlRegistryEvent::Global(name, interface, version)
+            )) => {
+                println!("[{:>4}] {} (version {})", name, interface, version);
+            },
+            // ignore everything else
+            _ => {}
+        }
     }
 }
