36340763e295fc9c34b05da7c1259bd590ea9376
Victor Berger <victor.berger@m4x.org>
2015-12-09 12:32:30 +0100

    Make travis use local crates.

---
+++ tweak-cargo-tomls.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sed -i.bak 's/wayland-sys = { version = "[0-9\.]*"/wayland-sys = { path = "..\/wayland-sys"/' ./wayland-client/Cargo.toml
+sed -i.bak 's/wayland-scanner = { version = "[0-9\.]*"/wayland-scanner = { path = "..\/scanner"/' ./wayland-client/Cargo.toml
