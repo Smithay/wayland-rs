6212b862c612e5fc6dd4e60efde0ac76c8a3a078
Victor Berger <victor.berger@m4x.org>
2016-02-11 21:53:33 +0100

    server: initial commit.

--- tweak-cargo-tomls.sh
+++ tweak-cargo-tomls.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 sed -i.bak 's/wayland-sys = { version = "[0-9\.\^]*"/wayland-sys = { path = "..\/wayland-sys"/' ./wayland-client/Cargo.toml
 sed -i.bak 's/wayland-scanner = { version = "[0-9\.\^]*"/wayland-scanner = { path = "..\/scanner"/' ./wayland-client/Cargo.toml
+sed -i.bak 's/wayland-sys = { version = "[0-9\.\^]*"/wayland-sys = { path = "..\/wayland-sys"/' ./wayland-server/Cargo.toml
+sed -i.bak 's/wayland-scanner = { version = "[0-9\.\^]*"/wayland-scanner = { path = "..\/scanner"/' ./wayland-server/Cargo.toml
