bc537ef993ce53f258766bd36045f7a516a19ae0
Victor Berger <victor.berger@m4x.org>
2016-10-02 19:24:59 +0200

    client: egl module
    Activated by "egl" cargo feature.

--- wayland-client/src/lib.rs
+++ wayland-client/src/lib.rs
@@ -134,6 +134,9 @@ mod display;
 mod event_queue;
 mod env;
 
+#[cfg(feature = "egl")]
+pub mod egl;
+
 pub use event_queue::{EventQueue, EventQueueHandle, StateGuard, Init};
 pub use display::{default_connect, ConnectError, FatalError};
 pub use env::{EnvHandler, EnvHandlerInner};
