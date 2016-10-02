c6c36d67d467de8aa1b639730507d434aa992031
Victor Berger <victor.berger@m4x.org>
2016-10-02 19:38:02 +0200

    client: libwayland-cursor bindings

--- wayland-client/src/lib.rs
+++ wayland-client/src/lib.rs
@@ -137,6 +137,9 @@ mod env;
 #[cfg(feature = "egl")]
 pub mod egl;
 
+#[cfg(feature = "cursor")]
+pub mod cursor;
+
 pub use event_queue::{EventQueue, EventQueueHandle, StateGuard, Init};
 pub use display::{default_connect, ConnectError, FatalError};
 pub use env::{EnvHandler, EnvHandlerInner};
