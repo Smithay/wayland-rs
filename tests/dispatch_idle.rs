35e4f7877c710351cc8c93f2e2ab3d92935cd707
Victor Berger <victor.berger@m4x.org>
2017-10-30 16:23:01 +0100

    server: dispatch idle test

---
+++ tests/dispatch_idle.rs
@@ -0,0 +1,22 @@
+extern crate wayland_client as wayc;
+extern crate wayland_server as ways;
+
+mod helpers;
+
+use helpers::TestServer;
+
+#[test]
+fn skel() {
+    // Server setup
+    //
+    let mut server = TestServer::new();
+
+    let idle = server
+        .event_loop
+        .add_idle_event_source(|_, idata| *idata = true, false);
+
+    server.event_loop.dispatch(Some(1)).unwrap();
+
+    let done = idle.remove();
+    assert!(done);
+}
