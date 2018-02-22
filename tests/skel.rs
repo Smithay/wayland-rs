2b1c5dbb96bc66c96e7d5b3a695eeb32feffe6f7
Victor Berger <victor.berger@m4x.org>
2018-02-22 10:55:22 +0100

    Prepare rewrite, remove all that must go.

--- tests/skel.rs
+++ tests/skel.rs
@@ -1,25 +1,3 @@
-extern crate wayland_client as wayc;
-extern crate wayland_server as ways;
-
-mod helpers;
-
-use helpers::{roundtrip, TestClient, TestServer};
-
 #[test]
-fn skel() {
-    // Server setup
-    //
-    let mut server = TestServer::new();
-
-    // Client setup
-    //
-    let mut client = TestClient::new(&server.socket_name);
-
-    // Some message passing
-    //
-    roundtrip(&mut client, &mut server);
-
-    // Final asserts
-    //
-    assert!(true);
+fn it_works() {
 }
