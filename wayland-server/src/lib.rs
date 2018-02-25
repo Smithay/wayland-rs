6a7e7f0c0c618f30cb2fd433bc20d24592d153bd
Victor Berger <victor.berger@m4x.org>
2018-02-25 15:55:03 +0100

    Bases of codegen & enums

--- wayland-server/src/lib.rs
+++ wayland-server/src/lib.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate bitflags;
 extern crate libc;
 
 extern crate wayland_commons;
@@ -6,6 +8,21 @@ extern crate wayland_commons;
 extern crate wayland_sys;
 
 mod resource;
-pub use resource::{Resource, NewResource};
+pub use resource::{NewResource, Resource};
 
 struct Client;
+
+mod generated {
+    #![allow(dead_code, non_camel_case_types, unused_unsafe, unused_variables)]
+    #![allow(non_upper_case_globals, non_snake_case, unused_imports)]
+    #![allow(missing_docs)]
+
+    #[cfg(feature = "native_lib")]
+    mod c_interfaces {
+        include!(concat!(env!("OUT_DIR"), "/wayland_c_interfaces.rs"));
+    }
+    #[cfg(feature = "native_lib")]
+    mod c_api {
+        include!(concat!(env!("OUT_DIR"), "/wayland_c_api.rs"));
+    }
+}
