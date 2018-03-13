408cb146fa50f78158e92719bdb68d9c621c3d78
Victor Berger <victor.berger@m4x.org>
2018-03-13 18:03:08 +0100

    client: proxy wrapper and !send implementations

--- wayland-client/src/event_queue.rs
+++ wayland-client/src/event_queue.rs
@@ -1,4 +1,6 @@
 use std::io::{Error as IoError, Result as IoResult};
+use std::ptr;
+use std::rc::Rc;
 use std::sync::Arc;
 
 use display::DisplayInner;
@@ -6,16 +8,28 @@ use display::DisplayInner;
 #[cfg(feature = "native_lib")]
 use wayland_sys::client::*;
 
-pub struct EventQueue {
-    wlevq: Option<*mut wl_event_queue>,
+struct EventQueueInner {
+    #[cfg(feature = "native_lib")] wlevq: Option<*mut wl_event_queue>,
     inner: Arc<DisplayInner>,
 }
 
+pub struct EventQueue {
+    // EventQueue is *not* Send
+    inner: Rc<EventQueueInner>,
+}
+
+pub struct QueueToken {
+    inner: Rc<EventQueueInner>,
+}
+
 impl EventQueue {
+    #[cfg(feature = "native_lib")]
     pub(crate) unsafe fn new(inner: Arc<DisplayInner>, evq: Option<*mut wl_event_queue>) -> EventQueue {
         EventQueue {
-            inner: inner,
-            wlevq: evq,
+            inner: Rc::new(EventQueueInner {
+                inner: inner,
+                wlevq: evq,
+            }),
         }
     }
 
@@ -34,17 +48,21 @@ impl EventQueue {
         {}
         #[cfg(feature = "native_lib")]
         {
-            let ret = match self.wlevq {
+            let ret = match self.inner.wlevq {
                 Some(evq) => unsafe {
                     ffi_dispatch!(
                         WAYLAND_CLIENT_HANDLE,
                         wl_display_dispatch_queue,
-                        self.inner.ptr(),
+                        self.inner.inner.ptr(),
                         evq
                     )
                 },
                 None => unsafe {
-                    ffi_dispatch!(WAYLAND_CLIENT_HANDLE, wl_display_dispatch, self.inner.ptr())
+                    ffi_dispatch!(
+                        WAYLAND_CLIENT_HANDLE,
+                        wl_display_dispatch,
+                        self.inner.inner.ptr()
+                    )
                 },
             };
             if ret >= 0 {
@@ -68,12 +86,12 @@ impl EventQueue {
         {}
         #[cfg(feature = "native_lib")]
         {
-            let ret = match self.wlevq {
+            let ret = match self.inner.wlevq {
                 Some(evq) => unsafe {
                     ffi_dispatch!(
                         WAYLAND_CLIENT_HANDLE,
                         wl_display_dispatch_queue_pending,
-                        self.inner.ptr(),
+                        self.inner.inner.ptr(),
                         evq
                     )
                 },
@@ -81,7 +99,7 @@ impl EventQueue {
                     ffi_dispatch!(
                         WAYLAND_CLIENT_HANDLE,
                         wl_display_dispatch_pending,
-                        self.inner.ptr()
+                        self.inner.inner.ptr()
                     )
                 },
             };
@@ -108,17 +126,17 @@ impl EventQueue {
         #[cfg(feature = "native_lib")]
         {
             let ret = unsafe {
-                match self.wlevq {
+                match self.inner.wlevq {
                     Some(evtq) => ffi_dispatch!(
                         WAYLAND_CLIENT_HANDLE,
                         wl_display_roundtrip_queue,
-                        self.inner.ptr(),
+                        self.inner.inner.ptr(),
                         evtq
                     ),
                     None => ffi_dispatch!(
                         WAYLAND_CLIENT_HANDLE,
                         wl_display_roundtrip,
-                        self.inner.ptr()
+                        self.inner.inner.ptr()
                     ),
                 }
             };
@@ -129,9 +147,31 @@ impl EventQueue {
             }
         }
     }
+
+    pub fn get_token(&self) -> QueueToken {
+        QueueToken {
+            inner: self.inner.clone(),
+        }
+    }
 }
 
-impl Drop for EventQueue {
+impl QueueToken {
+    pub(crate) unsafe fn assign_proxy(&self, proxy: *mut wl_proxy) {
+        #[cfg(not(feature = "native_lib"))]
+        {}
+        #[cfg(feature = "native_lib")]
+        {
+            ffi_dispatch!(
+                WAYLAND_CLIENT_HANDLE,
+                wl_proxy_set_queue,
+                proxy,
+                self.inner.wlevq.unwrap_or(ptr::null_mut())
+            )
+        }
+    }
+}
+
+impl Drop for EventQueueInner {
     fn drop(&mut self) {
         #[cfg(feature = "nativel_lib")]
         {
--- wayland-client/src/proxy.rs
+++ wayland-client/src/proxy.rs
@@ -3,6 +3,8 @@ use wayland_commons::{Implementation, Interface, MessageGroup};
 #[cfg(feature = "native_lib")]
 use wayland_sys::client::*;
 
+use event_queue::QueueToken;
+
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, AtomicPtr, Ordering};
 
@@ -25,7 +27,11 @@ pub struct Proxy<I: Interface> {
     #[cfg(not(feature = "native_lib"))] internal: Arc<ProxyInternal>,
     #[cfg(feature = "native_lib")] internal: Option<Arc<ProxyInternal>>,
     #[cfg(feature = "native_lib")] ptr: *mut wl_proxy,
+    #[cfg(feature = "native_lib")] is_wrapper: bool,
 }
+
+unsafe impl<I: Interface> Send for Proxy<I> {}
+unsafe impl<I: Interface> Sync for Proxy<I> {}
 
 impl<I: Interface> Proxy<I> {
     pub fn send(&self, msg: I::Requests) {
@@ -112,6 +118,8 @@ impl<I: Interface> Proxy<I> {
             internal: self.internal.clone(),
             #[cfg(feature = "native_lib")]
             ptr: self.ptr,
+            #[cfg(feature = "native_lib")]
+            is_wrapper: self.is_wrapper,
         }
     }
 
@@ -139,19 +147,46 @@ impl<I: Interface> Proxy<I> {
             _i: ::std::marker::PhantomData,
             internal: internal,
             ptr: ptr,
+            is_wrapper: false,
         }
     }
 
     #[doc(hidden)]
+    #[cfg(feature = "native_lib")]
     pub unsafe fn new_null() -> Proxy<I> {
         Proxy {
             _i: ::std::marker::PhantomData,
             internal: None,
             ptr: ::std::ptr::null_mut(),
+            is_wrapper: false,
         }
     }
 
+    #[cfg(feature = "native_lib")]
+    pub fn make_wrapper(&self, queue: &QueueToken) -> Result<Proxy<I>, ()> {
+        if !self.is_external() && !self.is_alive() {
+            return Err(());
+        }
+
+        let wrapper_ptr;
+        unsafe {
+            wrapper_ptr = ffi_dispatch!(WAYLAND_CLIENT_HANDLE, wl_proxy_create_wrapper, self.ptr);
+            queue.assign_proxy(wrapper_ptr);
+        }
+
+        Ok(Proxy {
+            _i: ::std::marker::PhantomData,
+            internal: self.internal.clone(),
+            ptr: wrapper_ptr,
+            is_wrapper: true,
+        })
+    }
+
     pub fn child<C: Interface>(&self) -> NewProxy<C> {
+        #[cfg(not(feature = "native_lib"))]
+        {
+            unimplemented!()
+        }
         #[cfg(feature = "native_lib")]
         {
             let ptr = unsafe {
@@ -177,6 +212,20 @@ impl Proxy<::protocol::wl_display::WlDisplay> {
             _i: ::std::marker::PhantomData,
             internal: None,
             ptr: d as *mut wl_proxy,
+            is_wrapper: false,
+        }
+    }
+}
+
+impl<I: Interface> Drop for Proxy<I> {
+    fn drop(&mut self) {
+        #[cfg(feature = "native_lib")]
+        {
+            if self.is_wrapper {
+                unsafe {
+                    ffi_dispatch!(WAYLAND_CLIENT_HANDLE, wl_proxy_wrapper_destroy, self.ptr);
+                }
+            }
         }
     }
 }
@@ -187,7 +236,44 @@ pub struct NewProxy<I: Interface> {
 }
 
 impl<I: Interface + 'static> NewProxy<I> {
-    pub fn implement<ID: 'static>(
+    /// Implement this proxy using given function and implementation data.
+    pub fn implement<ID: 'static + Send>(
+        self,
+        idata: ID,
+        implementation: Implementation<Proxy<I>, I::Events, ID>,
+    ) -> Proxy<I> {
+        unsafe { self.implement_inner(idata, implementation) }
+    }
+
+    /// Implement this proxy using given function and implementation data.
+    ///
+    /// This method allows the implementation data to not be `Send`, but requires for
+    /// safety that you provide a token to the event queue this proxy will be implemented
+    /// on. This method will then ensure that this proxy is registered on this event queue,
+    /// so that it cannot be dispatched from an other thread.
+    ///
+    /// **Unsafety:**
+    ///
+    /// This call can be racy if the proxy is not already registered on this event queue and its
+    /// old queue is being dispatched from an other thread.
+    ///
+    /// To ensure safety, see `Proxy::make_wrapper`.
+    pub unsafe fn implement_nonsend<ID: 'static>(
+        self,
+        idata: ID,
+        implementation: Implementation<Proxy<I>, I::Events, ID>,
+        queue: &QueueToken,
+    ) -> Proxy<I> {
+        #[cfg(not(feature = "native_lib"))]
+        {}
+        #[cfg(feature = "native_lib")]
+        {
+            queue.assign_proxy(self.c_ptr());
+            self.implement_inner(idata, implementation)
+        }
+    }
+
+    unsafe fn implement_inner<ID: 'static>(
         self,
         idata: ID,
         implementation: Implementation<Proxy<I>, I::Events, ID>,
@@ -206,21 +292,20 @@ impl<I: Interface + 'static> NewProxy<I> {
             ));
             let internal = new_user_data.internal.clone();
 
-            unsafe {
-                ffi_dispatch!(
-                    WAYLAND_CLIENT_HANDLE,
-                    wl_proxy_add_dispatcher,
-                    self.ptr,
-                    self::native_machinery::proxy_dispatcher::<I, ID>,
-                    &::wayland_sys::RUST_MANAGED as *const _ as *const _,
-                    Box::into_raw(new_user_data) as *mut _
-                );
-            }
+            ffi_dispatch!(
+                WAYLAND_CLIENT_HANDLE,
+                wl_proxy_add_dispatcher,
+                self.ptr,
+                self::native_machinery::proxy_dispatcher::<I, ID>,
+                &::wayland_sys::RUST_MANAGED as *const _ as *const _,
+                Box::into_raw(new_user_data) as *mut _
+            );
 
             Proxy {
                 _i: ::std::marker::PhantomData,
                 internal: Some(internal),
                 ptr: self.ptr,
+                is_wrapper: false,
             }
         }
     }
