02e81d6893c90f4ebfa93c70991fa3ab1fd12ff7
Victor Berger <victor.berger@m4x.org>
2018-04-04 20:15:28 +0200

    tests: skeletton

--- tests/skel.rs
+++ tests/skel.rs
@@ -1,2 +1,22 @@
+mod helpers;
+
+use helpers::{roundtrip, TestClient, TestServer};
+
 #[test]
-fn it_works() {}
+fn skel() {
+    // Server setup
+    //
+    let mut server = TestServer::new();
+
+    // Client setup
+    //
+    let mut client = TestClient::new(&server.socket_name);
+
+    // Some message passing
+    //
+    roundtrip(&mut client, &mut server);
+
+    // Final asserts
+    //
+    assert!(true);
+}
