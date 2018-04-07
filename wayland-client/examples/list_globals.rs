a26976241b86403b16be64f4385e2dec72d361d8
Victor Berger <victor.berger@m4x.org>
2018-04-07 20:13:38 +0200

    client: examples

--- wayland-client/examples/list_globals.rs
+++ wayland-client/examples/list_globals.rs
@@ -1,30 +1,27 @@
 extern crate wayland_client;
 
-use wayland_client::Display;
+use wayland_client::{Display, GlobalManager};
 
 use wayland_client::protocol::wl_display::RequestsTrait;
-use wayland_client::protocol::wl_registry::Events as RegistryEvents;
+
+// A minimal example printing the list of globals abvertized by the server and
+// then exiting
 
 fn main() {
+    // Connect to the server
     let (display, mut event_queue) = Display::connect_to_env().unwrap();
 
-    let registry = display
-        .get_registry()
-        .unwrap()
-        .implement(|evt, _| match evt {
-            RegistryEvents::Global {
-                name,
-                interface,
-                version,
-            } => {
-                println!(
-                    "New global with id {} and version {} of interface '{}'.",
-                    name, version, interface
-                );
-            }
-            _ => {}
-        });
+    // We use the GlobalManager convenience provided by the crate, it covers
+    // most classic use cases and avoids us the trouble to manually implement
+    // the registry
+    let globals = GlobalManager::new(display.get_registry().unwrap());
 
+    // A roundtrip synchronization to make sure the server received our registry
+    // creation and sent us the global list
     event_queue.sync_roundtrip().unwrap();
-    event_queue.sync_roundtrip().unwrap();
+
+    // Print the list
+    for (id, interface, version) in globals.list() {
+        println!("{}: {} (version {})", id, interface, version);
+    }
 }
