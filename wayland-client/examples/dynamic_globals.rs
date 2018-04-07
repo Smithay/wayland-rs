65c08229f5ebcb40434836baf1a0cbd4efec8a6b
Victor Berger <victor.berger@m4x.org>
2018-04-07 20:13:38 +0200

    client: examples

---
+++ wayland-client/examples/dynamic_globals.rs
@@ -0,0 +1,123 @@
+#[macro_use]
+extern crate wayland_client;
+
+use wayland_client::{Display, GlobalManager};
+
+use wayland_client::protocol::wl_display::RequestsTrait;
+use wayland_client::protocol::{wl_output, wl_seat};
+
+// An example showcasing the capability of GlobalManager to handle
+// dynamically created globals like wl_seat or wl_output, which can
+// exist with multiplicity and created at any time
+
+fn main() {
+    let (display, mut event_queue) = Display::connect_to_env().unwrap();
+
+    // We create a GlobalManager with a callback, that will be
+    // advertized of any global creation or deletion
+    let _globals = GlobalManager::new_with_cb(
+        display.get_registry().unwrap(),
+        // This macro generates a callback for auto-creating the globals
+        // that interest us and calling our provided callbacks
+        global_filter!(
+            // Here we ask that all seats be automatically instanciated
+            // with version 1 when advertized, and provide a callback that
+            // will handle the created wl_seat to implement them
+            //
+            // NOTE: the type annotations are necessayr because rustc's
+            // inference is apparently not smart enough
+            [
+                wl_seat::WlSeat,
+                1,
+                |seat: Result<NewProxy<_>, _>, ()| {
+                    // here seat is a result, as failure can happen if the server
+                    // advertized an lower version than we requested.
+                    // This cannot happer here as we requested version 1
+                    let seat = seat.unwrap();
+                    let mut seat_name = None;
+                    let mut caps = None;
+                    seat.implement(move |event, _| {
+                        use wayland_client::protocol::wl_seat::Events;
+                        match event {
+                            Events::Name { name } => {
+                                seat_name = Some(name);
+                            }
+                            Events::Capabilities { capabilities } => {
+                                // We *should* have received the "name" event first
+                                caps = Some(capabilities);
+                            }
+                        }
+                        if let (&Some(ref caps), &Some(ref name)) = (&caps, &seat_name) {
+                            println!("New seat \"{}\" with capabilities [ {:?} ]", name, caps);
+                        }
+                    });
+                }
+            ],
+            // Same thing with wl_output, but we require version 2
+            [
+                wl_output::WlOutput,
+                2,
+                |output: Result<NewProxy<_>, _>, ()| {
+                    let output = output.unwrap();
+                    let mut name = "<unknown>".to_owned();
+                    let mut modes = Vec::new();
+                    let mut scale = 1;
+                    output.implement(move |event, _| {
+                        use wayland_client::protocol::wl_output::Events;
+                        match event {
+                            Events::Geometry {
+                                x,
+                                y,
+                                physical_width,
+                                physical_height,
+                                subpixel,
+                                make,
+                                model,
+                                transform,
+                            } => {
+                                println!("New output: \"{} ({})\"", make, model);
+                                println!(
+                                    " -> physical dimensions {}x{}",
+                                    physical_width, physical_height
+                                );
+                                println!(" -> location in the compositor space: ({}, {})", x, y);
+                                println!(" -> transform: {:?}", transform);
+                                println!(" -> subpixel orientation: {:?}", subpixel);
+                                name = format!("{} ({})", make, model);
+                            }
+                            Events::Mode {
+                                flags,
+                                width,
+                                height,
+                                refresh,
+                            } => {
+                                modes.push((flags, width, height, refresh));
+                            }
+                            Events::Scale { factor } => {
+                                scale = factor;
+                            }
+                            Events::Done => {
+                                println!("Modesetting information for output \"{}\"", name);
+                                println!(" -> scaling factor: {}", scale);
+                                println!(" -> mode list:");
+                                for &(f, w, h, r) in &modes {
+                                    println!(
+                                        "   -> {}x{} @{}Hz (flags: [ {:?} ])",
+                                        w,
+                                        h,
+                                        (r as f32) / 1000.0,
+                                        f
+                                    );
+                                }
+                            }
+                        }
+                    });
+                }
+            ]
+        ),
+    );
+
+    event_queue.sync_roundtrip().unwrap();
+    event_queue.sync_roundtrip().unwrap();
+    event_queue.sync_roundtrip().unwrap();
+}
---
+++ wayland-client/examples/simple_window.rs
@@ -0,0 +1,161 @@
+extern crate byteorder;
+extern crate tempfile;
+extern crate wayland_client;
+
+use std::cmp::min;
+use std::io::Write;
+use std::os::unix::io::AsRawFd;
+
+use byteorder::{NativeEndian, WriteBytesExt};
+
+use wayland_client::{Display, GlobalManager, Proxy};
+use wayland_client::protocol::{wl_compositor, wl_seat, wl_shell, wl_shell_surface, wl_shm};
+use wayland_client::protocol::wl_display::RequestsTrait as DisplayRequests;
+use wayland_client::protocol::wl_compositor::RequestsTrait as CompositorRequests;
+use wayland_client::protocol::wl_surface::RequestsTrait as SurfaceRequests;
+use wayland_client::protocol::wl_shm::RequestsTrait as ShmRequests;
+use wayland_client::protocol::wl_shm_pool::RequestsTrait as PoolRequests;
+use wayland_client::protocol::wl_shell::RequestsTrait as ShellRequests;
+use wayland_client::protocol::wl_shell_surface::RequestsTrait as ShellSurfaceRequests;
+
+fn main() {
+    let (display, mut event_queue) = Display::connect_to_env().unwrap();
+    let globals = GlobalManager::new(display.get_registry().unwrap());
+
+    // roundtrip to retrieve the globals list
+    event_queue.sync_roundtrip().unwrap();
+
+    /*
+     * Create a buffer with window contents
+     */
+
+    // buffer (and window) width and height
+    let buf_x: u32 = 320;
+    let buf_y: u32 = 240;
+
+    // create a tempfile to write the conents of the window on
+    let mut tmp = tempfile::tempfile()
+        .ok()
+        .expect("Unable to create a tempfile.");
+    // write the contents to it, lets put a nice color gradient
+    for i in 0..(buf_x * buf_y) {
+        let x = (i % buf_x) as u32;
+        let y = (i / buf_x) as u32;
+        let r: u32 = min(((buf_x - x) * 0xFF) / buf_x, ((buf_y - y) * 0xFF) / buf_y);
+        let g: u32 = min((x * 0xFF) / buf_x, ((buf_y - y) * 0xFF) / buf_y);
+        let b: u32 = min(((buf_x - x) * 0xFF) / buf_x, (y * 0xFF) / buf_y);
+        let _ = tmp.write_u32::<NativeEndian>((0xFF << 24) + (r << 16) + (g << 8) + b);
+    }
+    let _ = tmp.flush();
+
+    /*
+     * Init wayland objects
+     */
+
+    // The compositor allows us to creates surfaces
+    let compositor = globals
+        .instanciate::<wl_compositor::WlCompositor>(1)
+        .unwrap()
+        .implement(|_, _| {});
+    let surface = compositor.create_surface().unwrap().implement(|_, _| {});
+
+    // The SHM allows us to share memory with the server, and create buffers
+    // on this shared memory to paint our surfaces
+    let shm = globals
+        .instanciate::<wl_shm::WlShm>(1)
+        .unwrap()
+        .implement(|_, _| {});
+    let pool = shm.create_pool(
+        tmp.as_raw_fd(),            // RawFd to the tempfile serving as shared memory
+        (buf_x * buf_y * 4) as i32, // size in bytes of the shared memory (4 bytes per pixel)
+    ).unwrap()
+        .implement(|_, _| {});
+    let buffer = pool.create_buffer(
+        0,                        // Start of the buffer in the pool
+        buf_x as i32,             // width of the buffer in pixels
+        buf_y as i32,             // height of the buffer in pixels
+        (buf_x * 4) as i32,       // number of bytes between the beginning of two consecutive lines
+        wl_shm::Format::Argb8888, // chosen encoding for the data
+    ).unwrap()
+        .implement(|_, _| {});
+
+    // The shell allows us to define our surface as a "toplevel", meaning the
+    // server will treat it as a window
+    //
+    // NOTE: the wl_shell interface is actually deprecated in favour of the xdg_shell
+    // protocol, available in wayland-protocols. But this will do for this example.
+    let shell = globals
+        .instanciate::<wl_shell::WlShell>(1)
+        .unwrap()
+        .implement(|_, _| {});
+    let shell_surface = shell.get_shell_surface(&surface).unwrap().implement(
+        |event, shell_surface: Proxy<wl_shell_surface::WlShellSurface>| {
+            use wayland_client::protocol::wl_shell_surface::{Events, RequestsTrait};
+            // This ping/pong mechanism is used by the wayland server to detect
+            // unresponsive applications
+            if let Events::Ping { serial } = event {
+                shell_surface.pong(serial);
+            }
+        },
+    );
+
+    // Set our surface as toplevel and define its contents
+    shell_surface.set_toplevel();
+    surface.attach(Some(&buffer), 0, 0);
+    surface.commit();
+
+    // initialize a seat to retrieve pointer events
+    // to be handled properly this should be more dynamic, as more
+    // than one seat can exist (and they can be created and destroyed
+    // dynamically), however most "traditional" setups have a single
+    // seat, so we'll keep it simple here
+    let mut pointer_created = false;
+    let _seat = globals
+        .instanciate::<wl_seat::WlSeat>(1)
+        .unwrap()
+        .implement(move |event, seat: Proxy<wl_seat::WlSeat>| {
+            // The capabilities of a seat are known at runtime and we retrieve
+            // them via an events. 3 capabilities exists: pointer, keyboard, and touch
+            // we are only interested in pointer here
+            use wayland_client::protocol::wl_seat::{Capability, Events as SeatEvents,
+                                                    RequestsTrait as SeatRequests};
+            use wayland_client::protocol::wl_pointer::Events as PointerEvents;
+
+            if let SeatEvents::Capabilities { capabilities } = event {
+                if !pointer_created && capabilities.contains(Capability::Pointer) {
+                    // create the pointer only once
+                    pointer_created = true;
+                    seat.get_pointer()
+                        .unwrap()
+                        .implement(|event, _| match event {
+                            PointerEvents::Enter {
+                                surface_x,
+                                surface_y,
+                                ..
+                            } => {
+                                println!("Pointer entered at ({}, {}).", surface_x, surface_y);
+                            }
+                            PointerEvents::Leave { .. } => {
+                                println!("Pointer left.");
+                            }
+                            PointerEvents::Motion {
+                                surface_x,
+                                surface_y,
+                                ..
+                            } => {
+                                println!("Pointer moved to ({}, {}).", surface_x, surface_y);
+                            }
+                            PointerEvents::Button { button, state, .. } => {
+                                println!("Button {} was {:?}.", button, state);
+                            }
+                            _ => {}
+                        });
+                }
+            }
+        });
+
+    loop {
+        display.flush().unwrap();
+        event_queue.dispatch().unwrap();
+    }
+}
