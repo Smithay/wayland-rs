798bcc72c25b1d2a0e8a23746ffa54a80309564c
Victor Berger <victor.berger@m4x.org>
2018-04-07 09:29:30 +0200

    more tests

---
+++ tests/attach_null_to_surface.rs
@@ -0,0 +1,78 @@
+use std::sync::{Arc, Mutex};
+
+mod helpers;
+
+use helpers::{roundtrip, wayc, ways, TestClient, TestServer};
+
+use wayc::protocol::wl_display::RequestsTrait as DisplayRequests;
+use wayc::protocol::wl_compositor::RequestsTrait as CompositorRequests;
+use wayc::protocol::wl_surface::RequestsTrait as SurfaceRequests;
+
+fn insert_compositor(server: &mut TestServer) -> Arc<Mutex<bool>> {
+    use ways::protocol::{wl_compositor, wl_surface};
+    use ways::NewResource;
+
+    let seen_surface = Arc::new(Mutex::new(false));
+    let seen_surface2 = seen_surface.clone();
+
+    let loop_token = server.event_loop.token();
+    server
+        .display
+        .create_global::<wl_compositor::WlCompositor, _>(
+            &loop_token,
+            1,
+            move |version, compositor: NewResource<_>| {
+                let compositor_seen_surface = seen_surface.clone();
+                compositor.implement(
+                    move |event, _| {
+                        if let wl_compositor::Requests::CreateSurface { id: surface } = event {
+                            let my_seen_surface = compositor_seen_surface.clone();
+                            surface.implement(
+                                move |event, _| {
+                                    if let wl_surface::Requests::Attach { buffer, x, y } = event {
+                                        assert!(buffer.is_none());
+                                        *(my_seen_surface.lock().unwrap()) = true;
+                                    } else {
+                                        panic!("Unexpected event on surface!");
+                                    }
+                                },
+                                None::<fn(_, _)>,
+                            );
+                        } else {
+                            panic!("Unexpected event on compositor!");
+                        }
+                    },
+                    None::<fn(_, _)>,
+                );
+            },
+        );
+
+    seen_surface2
+}
+
+#[test]
+fn attach_null() {
+    // Server setup
+    //
+    let mut server = TestServer::new();
+    let seen_surface = insert_compositor(&mut server);
+
+    // Client setup
+    //
+    let mut client = TestClient::new(&server.socket_name);
+    let manager = wayc::GlobalManager::new(client.display.get_registry().unwrap());
+
+    // Initial sync
+    roundtrip(&mut client, &mut server);
+
+    let compositor = manager
+        .instanciate::<wayc::protocol::wl_compositor::WlCompositor>(1)
+        .unwrap()
+        .implement(|_, _| {});
+    let surface = compositor.create_surface().unwrap().implement(|_, _| {});
+    surface.attach(None, 0, 0);
+
+    roundtrip(&mut client, &mut server);
+
+    assert!(*seen_surface.lock().unwrap());
+}
---
+++ tests/basic_global.rs
@@ -0,0 +1,54 @@
+mod helpers;
+
+use helpers::{roundtrip, wayc, ways, TestClient, TestServer};
+
+use ways::protocol::wl_compositor::WlCompositor as ServerCompositor;
+use wayc::protocol::wl_display::RequestsTrait;
+
+#[test]
+fn simple_global() {
+    let mut server = TestServer::new();
+    let loop_token = server.event_loop.token();
+    server
+        .display
+        .create_global::<ServerCompositor, _>(&loop_token, 1, |_, _| {});
+
+    let mut client = TestClient::new(&server.socket_name);
+    let manager = wayc::GlobalManager::new(client.display.get_registry().unwrap());
+
+    roundtrip(&mut client, &mut server);
+    let globals = manager.list();
+    assert!(globals.len() == 1);
+    assert_eq!(globals[0], (1, "wl_compositor".into(), 1));
+}
+
+#[test]
+fn multi_versions() {
+    let mut server = TestServer::new();
+    let loop_token = server.event_loop.token();
+    server
+        .display
+        .create_global::<ServerCompositor, _>(&loop_token, 4, |_, _| {});
+    server
+        .display
+        .create_global::<ServerCompositor, _>(&loop_token, 3, |_, _| {});
+    server
+        .display
+        .create_global::<ServerCompositor, _>(&loop_token, 2, |_, _| {});
+    server
+        .display
+        .create_global::<ServerCompositor, _>(&loop_token, 1, |_, _| {});
+
+    let mut client = TestClient::new(&server.socket_name);
+    let manager = wayc::GlobalManager::new(client.display.get_registry().unwrap());
+
+    roundtrip(&mut client, &mut server);
+    let globals = manager.list();
+    assert!(globals.len() == 4);
+    let mut seen = [false; 4];
+    for &(_, ref interface, version) in &globals {
+        assert!(interface == "wl_compositor");
+        seen[version as usize - 1] = true;
+    }
+    assert_eq!(seen, [true, true, true, true]);
+}
