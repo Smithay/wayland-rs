f59a651e0314d280a2ca85b1d652cf868cd376e0
Victor Berger <victor.berger@m4x.org>
2021-02-26 11:08:55 +0100

    commons: core interfaces

---
+++ wayland-commons/src/core_interfaces.rs
@@ -0,0 +1,92 @@
+//! Core interfaces of teh protocol
+//!
+//! This module contains hard-coded interfaces for `wl_display`, `wl_registry` and `wl_callback`.
+//! These interfaces are frozen in the protocol and can never change. They are the only interfaces
+//! which the backends need to be aware of in particular.
+
+use super::{Interface, MessageDesc, ArgumentType};
+
+/// Interface `wl_display`
+pub static WL_DISPLAY_INTERFACE: Interface = Interface {
+    name: "wl_display",
+    version: 1,
+    requests: &[
+        MessageDesc {
+            name: "sync",
+            since: 1,
+            is_destructor: false,
+            signature: &[ArgumentType::NewId],
+            child_interface: Some(&WL_CALLBACK_INTERFACE)
+        },
+        MessageDesc {
+            name: "get_registry",
+            since: 1,
+            is_destructor: false,
+            signature: &[ArgumentType::NewId],
+            child_interface: Some(&WL_REGISTRY_INTERFACE)
+        }
+    ],
+    events: &[
+        MessageDesc {
+            name: "error",
+            since: 1,
+            is_destructor: false,
+            signature: &[ArgumentType::Object, ArgumentType::Uint, ArgumentType::Str],
+            child_interface: None
+        },
+        MessageDesc {
+            name: "delete_id",
+            since: 1,
+            is_destructor: false,
+            signature: &[ArgumentType::Uint],
+            child_interface: None
+        }
+    ]
+};
+
+/// Interface `wl_registry`
+pub static WL_REGISTRY_INTERFACE: Interface = Interface {
+    name: "wl_registry",
+    version: 1,
+    requests: &[
+        MessageDesc {
+            name: "bind",
+            since: 1,
+            is_destructor: false,
+            signature: &[ArgumentType::Uint, ArgumentType::NewId],
+            child_interface: None
+        }
+    ],
+    events: &[
+        MessageDesc {
+            name: "global",
+            since: 1,
+            is_destructor: false,
+            signature: &[ArgumentType::Uint, ArgumentType::Str, ArgumentType::Uint],
+            child_interface: None
+        },
+        MessageDesc {
+            name: "global_remove",
+            since: 1,
+            is_destructor: false,
+            signature: &[ArgumentType::Uint],
+            child_interface: None
+        }
+    ]
+};
+
+/// Interface `wl_callback`
+pub static WL_CALLBACK_INTERFACE: Interface = Interface {
+    name: "wl_callback",
+    version: 1,
+    requests: &[],
+    events: &[
+        MessageDesc {
+            name: "done",
+            since: 1,
+            is_destructor: true,
+            signature: &[ArgumentType::Uint],
+            child_interface: None
+        }
+    ]
+};
