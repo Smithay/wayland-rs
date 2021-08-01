122bf4f5dd2862092dc77cbcd485abf7cf384046
Victor Berger <victor.berger@m4x.org>
2021-08-01 17:48:56 +0200

    client: dispatch utilities

--- wayland-client/examples/list_globals.rs
+++ wayland-client/examples/list_globals.rs
@@ -1,48 +1,17 @@
-use std::sync::{atomic::AtomicBool, Arc};
+use std::sync::Arc;
 
-use wayland_client::{
-    backend::WaylandError,
-    convert_event,
-    protocol::{wl_callback, wl_registry},
-    proxy_internals::ProxyData,
-    Connection,
-};
+use wayland_client::{oneshot_sink, protocol::wl_registry, Connection};
 
 fn main() {
     let cx = Connection::connect_to_env().unwrap();
 
     let display = cx.handle().display();
 
-    let registry_data = ProxyData::new(Arc::new(|cx, msg| {
-        let (_registry, event) = convert_event!(cx, msg; wl_registry::WlRegistry).unwrap();
+    let registry_data = oneshot_sink!(wl_registry::WlRegistry, move |_cx, _registry, event| {
         eprintln!("{:?}", event);
-    }));
+    });
 
     let _registry = display.get_registry(&mut cx.handle(), Some(registry_data)).unwrap();
 
-    let done = Arc::new(AtomicBool::new(false));
-    let done2 = done.clone();
-
-    let callback_data = ProxyData::new(Arc::new(move |cx, msg| {
-        let (_callback, event) = convert_event!(cx, msg; wl_callback::WlCallback).unwrap();
-        eprintln!("{:?}", event);
-        done2.store(true, std::sync::atomic::Ordering::SeqCst);
-    }));
-
-    let _callback = display.sync(&mut cx.handle(), Some(callback_data)).unwrap();
-    cx.flush().unwrap();
-
-    while !done.load(std::sync::atomic::Ordering::SeqCst) {
-        match cx.dispatch_events() {
-            Ok(_) => {}
-            Err(WaylandError::Protocol(e)) => panic!("Protocol error: {:?}", e),
-            Err(WaylandError::Io(e)) => {
-                if e.kind() == std::io::ErrorKind::WouldBlock {
-                    continue;
-                } else {
-                    panic!("IO error: {:?}", e);
-                }
-            }
-        }
-    }
+    cx.roundtrip().unwrap();
 }
