d5fbe3a942029356f0647a4da59cec848fbc9162
Victor Berger <victor.berger@m4x.org>
2021-08-14 00:14:23 +0200

    client: window example

--- wayland-client/examples/list_globals.rs
+++ wayland-client/examples/list_globals.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use wayland_client::{quick_sink, protocol::wl_registry, Connection};
+use wayland_client::{protocol::wl_registry, quick_sink, Connection};
 
 fn main() {
     let cx = Connection::connect_to_env().unwrap();
@@ -8,10 +8,14 @@ fn main() {
     let display = cx.handle().display();
 
     let registry_data = quick_sink!(wl_registry::WlRegistry, move |_cx, (_registry, event)| {
-        eprintln!("{:?}", event);
+        if let wl_registry::Event::Global { name, interface, version } = event {
+            eprintln!("[{}] {} (v{})", name, interface, version);
+        }
     });
 
     let _registry = display.get_registry(&mut cx.handle(), Some(registry_data)).unwrap();
+
+    eprintln!("Advertized globals:");
 
     cx.roundtrip().unwrap();
 }
