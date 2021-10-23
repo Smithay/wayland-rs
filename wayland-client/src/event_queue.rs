74993757e4b0a196dabbd5be530ce7cc469639a0
Victor Berger <victor.berger@m4x.org>
2021-10-23 14:10:11 +0200

    client: Trait-based API

--- wayland-client/src/event_queue.rs
+++ wayland-client/src/event_queue.rs
@@ -1,118 +1,107 @@
-use std::{
-    cell::RefCell,
-    rc::Rc,
-    sync::{Arc, Mutex},
-};
+use std::sync::{Arc, Mutex};
 
 use futures_channel::mpsc::{unbounded, UnboundedReceiver, UnboundedSender};
-use slotmap::{DefaultKey, SlotMap};
 use wayland_backend::{
-    client::{Backend, ObjectId},
-    protocol::Message,
+    client::{Backend, Handle, ObjectData, ObjectId},
+    protocol::{Message, ObjectInfo},
 };
 
-use crate::{proxy_internals::ProxyData, ConnectionHandle, DispatchError, Proxy};
+use crate::{ConnectionHandle, DispatchError, Proxy};
 
 pub trait Dispatch<I: Proxy>: Sized {
     type UserData: Default + Send + Sync + 'static;
 
     fn event(
         &mut self,
-        proxy: I,
+        proxy: &I,
         event: I::Event,
         data: &Self::UserData,
         cxhandle: &mut ConnectionHandle,
         qhandle: &QueueHandle<Self>,
     );
 
-    fn destroyed(&mut self, _proxy: I, _data: &Self::UserData) {}
+    fn destroyed(&mut self, _proxy: &I, _data: &Self::UserData) {}
+
+    fn child_from_event(_: &ObjectInfo, _: &QueueHandle<Self>) -> Arc<dyn ObjectData> {
+        panic!(
+            "Attempting to create an object in event from uninitialized Dispatch<{}>",
+            std::any::type_name::<I>()
+        );
+    }
 }
 
-#[derive(Debug)]
-enum QueueEvent {
-    Msg(DefaultKey, Message<ObjectId>),
-    SinkDropped(DefaultKey),
+#[macro_export]
+macro_rules! generate_child_from_event {
+    ($($child_iface:ty),*) => {
+        fn child_from_event(info: &$crate::backend::protocol::ObjectInfo, handle: &$crate::QueueHandle<Self>) -> std::sync::Arc<dyn $crate::backend::ObjectData> {
+            match () {
+                $(
+                    () if $crate::backend::protocol::same_interface(info.interface, <$child_iface as $crate::Proxy>::interface()) => {
+                        handle.make_data::<$child_iface>()
+                    },
+                )*
+                _ => panic!("Attempting to create an unexpected object {:?} in event from Dispatch<{}>", info, std::any::type_name::<Self>()),
+            }
+        }
+    }
 }
 
-type InnerCallback<D> = dyn FnMut(
+type QueueCallback<D> = fn(
     &mut ConnectionHandle<'_>,
     Message<ObjectId>,
     &mut D,
     &QueueHandle<D>,
 ) -> Result<(), DispatchError>;
+type QueueDestructor<D> = fn(&mut ConnectionHandle<'_>, ObjectId, &mut D);
 
-struct QueueCallback<Data>(Rc<RefCell<InnerCallback<Data>>>);
-
-impl<Data> Clone for QueueCallback<Data> {
-    fn clone(&self) -> Self {
-        QueueCallback(self.0.clone())
-    }
+enum QueueEvent<D> {
+    Msg(QueueCallback<D>, Message<ObjectId>),
+    Destructor(QueueDestructor<D>, ObjectId),
 }
 
-impl<D> QueueCallback<D> {
-    fn new<
-        F: FnMut(
-                &mut ConnectionHandle<'_>,
-                Message<ObjectId>,
-                &mut D,
-                &QueueHandle<D>,
-            ) -> Result<(), DispatchError>
-            + 'static,
-    >(
-        f: F,
-    ) -> Self {
-        QueueCallback(Rc::new(RefCell::new(f)) as Rc<_>)
-    }
-
-    fn invoke(
-        &self,
-        handle: &mut ConnectionHandle,
-        msg: Message<ObjectId>,
-        data: &mut D,
-        qhandle: &QueueHandle<D>,
-    ) -> Result<(), DispatchError> {
-        let mut guard = self.0.borrow_mut();
-        (*guard)(handle, msg, data, qhandle)
-    }
-}
-
-impl<D> std::fmt::Debug for QueueCallback<D> {
+impl<D> std::fmt::Debug for QueueEvent<D> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("EventQueueCallback{ .. }")
+        match self {
+            QueueEvent::Msg(_, ref msg) => {
+                f.debug_struct("QueueEvent::Msg").field("msg", msg).finish()
+            }
+            QueueEvent::Destructor(_, ref id) => {
+                f.debug_struct("QueueEvent::Destructor").field("id", id).finish()
+            }
+        }
     }
 }
 
-pub struct EventQueue<Data> {
-    rx: UnboundedReceiver<QueueEvent>,
-    handle: QueueHandle<Data>,
+pub struct EventQueue<D> {
+    rx: UnboundedReceiver<QueueEvent<D>>,
+    handle: QueueHandle<D>,
     backend: Arc<Mutex<Backend>>,
 }
 
-impl<Data> std::fmt::Debug for EventQueue<Data> {
+impl<D> std::fmt::Debug for EventQueue<D> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("EventQueue").field("rx", &self.rx).field("handle", &self.handle).finish()
+        f.debug_struct("EventQueue")
+            .field("rx", &self.rx)
+            .field("handle", &self.handle)
+            .finish_non_exhaustive()
     }
 }
 
-impl<Data> EventQueue<Data> {
+impl<D> EventQueue<D> {
     pub(crate) fn new(backend: Arc<Mutex<Backend>>) -> Self {
         let (tx, rx) = unbounded();
-        EventQueue {
-            rx,
-            handle: QueueHandle { tx, callbacks: Rc::new(RefCell::new(SlotMap::new())) },
-            backend,
-        }
+        EventQueue { rx, handle: QueueHandle { tx }, backend }
     }
 
-    pub fn handle(&self) -> QueueHandle<Data> {
+    pub fn handle(&self) -> QueueHandle<D> {
         self.handle.clone()
     }
 
-    pub fn dispatch_pending(&mut self, data: &mut Data) -> Result<usize, DispatchError> {
+    pub fn dispatch_pending(&mut self, data: &mut D) -> Result<usize, DispatchError> {
         Self::dispatching_impl(&mut self.backend.lock().unwrap(), &mut self.rx, &self.handle, data)
     }
 
-    pub fn blocking_dispatch(&mut self, data: &mut Data) -> Result<usize, DispatchError> {
+    pub fn blocking_dispatch(&mut self, data: &mut D) -> Result<usize, DispatchError> {
         let mut backend = self.backend.lock().unwrap();
         let dispatched = Self::dispatching_impl(&mut backend, &mut self.rx, &self.handle, data)?;
         if dispatched > 0 {
@@ -125,22 +114,21 @@ impl<Data> EventQueue<Data> {
 
     fn dispatching_impl(
         backend: &mut Backend,
-        rx: &mut UnboundedReceiver<QueueEvent>,
-        qhandle: &QueueHandle<Data>,
-        data: &mut Data,
+        rx: &mut UnboundedReceiver<QueueEvent<D>>,
+        qhandle: &QueueHandle<D>,
+        data: &mut D,
     ) -> Result<usize, DispatchError> {
         let mut handle = ConnectionHandle::from_handle(backend.handle());
         let mut dispatched = 0;
 
         while let Ok(Some(evt)) = rx.try_next() {
             match evt {
-                QueueEvent::SinkDropped(key) => {
-                    qhandle.callbacks.borrow_mut().remove(key);
-                }
-                QueueEvent::Msg(key, msg) => {
-                    let target_cb = qhandle.callbacks.borrow()[key].clone();
-                    target_cb.invoke(&mut handle, msg, data, qhandle)?;
+                QueueEvent::Msg(cb, msg) => {
+                    cb(&mut handle, msg, data, qhandle)?;
                     dispatched += 1;
+                }
+                QueueEvent::Destructor(cb, id) => {
+                    cb(&mut handle, id, data);
                 }
             }
         }
@@ -148,63 +136,91 @@ impl<Data> EventQueue<Data> {
     }
 }
 
-pub struct QueueHandle<Data> {
-    tx: UnboundedSender<QueueEvent>,
-    callbacks: Rc<RefCell<SlotMap<DefaultKey, QueueCallback<Data>>>>,
+pub struct QueueHandle<D> {
+    tx: UnboundedSender<QueueEvent<D>>,
 }
 
 impl<Data> std::fmt::Debug for QueueHandle<Data> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("QueueHandle")
-            .field("tx", &self.tx)
-            .field("callbacks", &self.callbacks)
-            .finish()
+        f.debug_struct("QueueHandle").field("tx", &self.tx).finish()
     }
 }
 
 impl<Data> Clone for QueueHandle<Data> {
     fn clone(&self) -> Self {
-        QueueHandle { tx: self.tx.clone(), callbacks: self.callbacks.clone() }
+        QueueHandle { tx: self.tx.clone() }
     }
 }
 
-struct QueueSender {
-    key: DefaultKey,
-    tx: UnboundedSender<QueueEvent>,
+pub(crate) struct QueueSender<D> {
+    func: QueueCallback<D>,
+    dest: QueueDestructor<D>,
+    pub(crate) handle: QueueHandle<D>,
 }
 
-impl QueueSender {
+impl<D> QueueSender<D> {
     fn send(&self, msg: Message<ObjectId>) {
-        if self.tx.unbounded_send(QueueEvent::Msg(self.key, msg)).is_err() {
+        if self.handle.tx.unbounded_send(QueueEvent::Msg(self.func, msg)).is_err() {
             log::error!("Event received for EventQueue after it was dropped.");
         }
     }
-}
 
-impl Drop for QueueSender {
-    fn drop(&mut self) {
-        let _ = self.tx.unbounded_send(QueueEvent::SinkDropped(self.key));
+    fn send_destroy(&self, id: ObjectId) {
+        let _ = self.handle.tx.unbounded_send(QueueEvent::Destructor(self.dest, id));
     }
 }
 
-impl<D> QueueHandle<D> {
-    pub fn make_data<I: Proxy>(&self) -> Arc<ProxyData<D::UserData>>
+impl<D: 'static> QueueHandle<D> {
+    pub fn make_data<I: Proxy + 'static>(&self) -> Arc<dyn ObjectData>
     where
         D: Dispatch<I>,
     {
-        let callback = QueueCallback::new(move |handle, msg, data: &mut D, qhandle| {
-            let (proxy, event) = I::parse_event(handle, msg)?;
-            let pdata = proxy.data::<D>().expect("Wrong user_data value for object");
-            data.event(proxy, event, &pdata.udata, handle, qhandle);
-            Ok(())
-        });
-
-        let key = self.callbacks.borrow_mut().insert(callback);
-        let sender = QueueSender { key, tx: self.tx.clone() };
-        let sink_callback = move |_: &mut ConnectionHandle<'_>, msg, _: &D::UserData| {
-            sender.send(msg);
+        let sender = QueueSender {
+            func: queue_callback::<I, D>,
+            dest: queue_destructor::<I, D>,
+            handle: self.clone(),
         };
+        Arc::new(QueueProxyData { sender, udata: Default::default() })
+    }
+}
 
-        ProxyData::new(Arc::new(sink_callback))
+fn queue_callback<I: Proxy, D: Dispatch<I> + 'static>(
+    handle: &mut ConnectionHandle<'_>,
+    msg: Message<ObjectId>,
+    data: &mut D,
+    qhandle: &QueueHandle<D>,
+) -> Result<(), DispatchError> {
+    let (proxy, event) = I::parse_event(handle, msg)?;
+    let udata = proxy.data::<D>().expect("Wrong user_data value for object");
+    data.event(&proxy, event, udata, handle, qhandle);
+    Ok(())
+}
+
+fn queue_destructor<I: Proxy, D: Dispatch<I> + 'static>(
+    handle: &mut ConnectionHandle<'_>,
+    id: ObjectId,
+    data: &mut D,
+) {
+    let proxy = I::from_id(handle, id).expect("Processing destructor of invalid id ?!");
+    let udata = proxy.data::<D>().expect("Wrong user_data value for object");
+    data.destroyed(&proxy, udata)
+}
+
+pub struct QueueProxyData<I: Proxy, D: Dispatch<I>> {
+    pub(crate) sender: QueueSender<D>,
+    pub udata: <D as Dispatch<I>>::UserData,
+}
+
+impl<I: Proxy + 'static, D: Dispatch<I> + 'static> ObjectData for QueueProxyData<I, D> {
+    fn make_child(self: Arc<Self>, child_info: &ObjectInfo) -> Arc<dyn ObjectData> {
+        D::child_from_event(child_info, &self.sender.handle)
+    }
+
+    fn event(&self, _: &mut Handle, msg: Message<ObjectId>) {
+        self.sender.send(msg);
+    }
+
+    fn destroyed(&self, object_id: ObjectId) {
+        self.sender.send_destroy(object_id);
     }
 }
