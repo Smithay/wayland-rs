62a1cd6705c9c2a323891cfe317ec2d93195d7eb
Victor Berger <victor.berger@m4x.org>
2021-10-29 17:47:31 +0200

    scanner: improve style of quote use

--- wayland-scanner/src/c_interfaces.rs
+++ wayland-scanner/src/c_interfaces.rs
@@ -1,8 +1,8 @@
 use std::cmp;
 use std::iter::repeat;
 
-use proc_macro2::{Ident, Literal, Span, TokenStream};
-use quote::quote;
+use proc_macro2::{Literal, TokenStream};
+use quote::{format_ident, quote};
 
 use crate::protocol::{Interface, Message, Protocol, Type};
 
@@ -27,7 +27,7 @@ pub(crate) fn generate_interfaces_prefix(protocol: &Protocol) -> TokenStream {
 
     let types_null_len = Literal::usize_unsuffixed(longest_nulls);
 
-    let nulls = repeat(quote!(NULLPTR as *const wayland_backend::protocol::wl_interface))
+    let nulls = repeat(quote! { NULLPTR as *const wayland_backend::protocol::wl_interface })
         .take(longest_nulls);
 
     quote! {
@@ -42,25 +42,25 @@ pub(crate) fn generate_interface(interface: &Interface) -> TokenStream {
     let requests = gen_messages(interface, &interface.requests, "requests");
     let events = gen_messages(interface, &interface.events, "events");
 
-    let interface_ident = Ident::new(&format!("{}_interface", interface.name), Span::call_site());
+    let interface_ident = format_ident!("{}_interface", interface.name);
     let name_value = null_terminated_byte_string_literal(&interface.name);
     let version_value = Literal::i32_unsuffixed(interface.version as i32);
     let request_count_value = Literal::i32_unsuffixed(interface.requests.len() as i32);
     let requests_value = if interface.requests.is_empty() {
-        quote!(NULLPTR as *const wayland_backend::protocol::wl_message)
+        quote! { NULLPTR as *const wayland_backend::protocol::wl_message }
     } else {
-        let requests_ident = Ident::new(&format!("{}_requests", interface.name), Span::call_site());
-        quote!(unsafe { &#requests_ident as *const _ })
+        let requests_ident = format_ident!("{}_requests", interface.name);
+        quote! { unsafe { &#requests_ident as *const _ } }
     };
     let event_count_value = Literal::i32_unsuffixed(interface.events.len() as i32);
     let events_value = if interface.events.is_empty() {
-        quote!(NULLPTR as *const wayland_backend::protocol::wl_message)
+        quote! { NULLPTR as *const wayland_backend::protocol::wl_message }
     } else {
-        let events_ident = Ident::new(&format!("{}_events", interface.name), Span::call_site());
-        quote!(unsafe { &#events_ident as *const _ })
+        let events_ident = format_ident!("{}_events", interface.name);
+        quote! { unsafe { &#events_ident as *const _ } }
     };
 
-    quote!(
+    quote! {
         #requests
         #events
 
@@ -72,7 +72,7 @@ pub(crate) fn generate_interface(interface: &Interface) -> TokenStream {
             event_count: #event_count_value,
             events: #events_value,
         };
-    )
+    }
 }
 
 fn gen_messages(interface: &Interface, messages: &[Message], which: &str) -> TokenStream {
@@ -84,18 +84,14 @@ fn gen_messages(interface: &Interface, messages: &[Message], which: &str) -> Tok
         if msg.all_null() {
             None
         } else {
-            let array_ident = Ident::new(
-                &format!("{}_{}_{}_types", interface.name, which, msg.name),
-                Span::call_site(),
-            );
+            let array_ident = format_ident!("{}_{}_{}_types", interface.name, which, msg.name);
             let array_len = Literal::usize_unsuffixed(msg.args.len());
             let array_values = msg.args.iter().map(|arg| match (arg.typ, &arg.interface) {
                 (Type::Object, &Some(ref inter)) | (Type::NewId, &Some(ref inter)) => {
-                    let interface_ident =
-                        Ident::new(&format!("{}_interface", inter), Span::call_site());
-                    quote!(unsafe { &#interface_ident as *const wayland_backend::protocol::wl_interface })
+                    let interface_ident =format_ident!("{}_interface", inter);
+                    quote! { unsafe { &#interface_ident as *const wayland_backend::protocol::wl_interface } }
                 }
-                _ => quote!(NULLPTR as *const wayland_backend::protocol::wl_interface),
+                _ => quote! { NULLPTR as *const wayland_backend::protocol::wl_interface },
             });
 
             Some(quote! {
@@ -106,20 +102,16 @@ fn gen_messages(interface: &Interface, messages: &[Message], which: &str) -> Tok
         }
     });
 
-    let message_array_ident =
-        Ident::new(&format!("{}_{}", interface.name, which), Span::call_site());
+    let message_array_ident = format_ident!("{}_{}", interface.name, which);
     let message_array_len = Literal::usize_unsuffixed(messages.len());
     let message_array_values = messages.iter().map(|msg| {
         let name_value = null_terminated_byte_string_literal(&msg.name);
         let signature_value = Literal::byte_string(&message_signature(msg));
 
         let types_ident = if msg.all_null() {
-            Ident::new("types_null", Span::call_site())
+            format_ident!("types_null")
         } else {
-            Ident::new(
-                &format!("{}_{}_{}_types", interface.name, which, msg.name),
-                Span::call_site(),
-            )
+            format_ident!("{}_{}_{}_types", interface.name, which, msg.name)
         };
 
         quote! {
