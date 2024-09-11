305bc5a2100070262b65839cbbb23de7a4c73a2e
ids1024 <ids1024@users.noreply.github.com>
2024-09-11 17:17:12 +0000

    deploy: 54be7ba016cb4eb9fc0553ece867768549182f6c

---
+++ trait.impl/wayland_backend/sys/server/trait.ClientData.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"wayland_backend":[],
+"wayland_server":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
