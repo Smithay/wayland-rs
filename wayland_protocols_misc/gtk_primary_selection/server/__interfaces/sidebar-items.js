fac290baadc7294a2883417a5374fb0fad4367ce
ids1024 <ids1024@users.noreply.github.com>
2024-12-24 18:21:49 +0000

    deploy: c65ff18c6babb67e52fe55f213d263ee7713e7f8

---
+++ wayland_protocols_misc/gtk_primary_selection/server/__interfaces/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"static":["GTK_PRIMARY_SELECTION_DEVICE_INTERFACE","GTK_PRIMARY_SELECTION_DEVICE_MANAGER_INTERFACE","GTK_PRIMARY_SELECTION_OFFER_INTERFACE","GTK_PRIMARY_SELECTION_SOURCE_INTERFACE","gtk_primary_selection_device_interface","gtk_primary_selection_device_manager_interface","gtk_primary_selection_offer_interface","gtk_primary_selection_source_interface"]};
---
+++ wayland_protocols_misc/zwp_input_method_v2/server/zwp_input_method_manager_v2/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["REQ_DESTROY_OPCODE","REQ_DESTROY_SINCE","REQ_GET_INPUT_METHOD_OPCODE","REQ_GET_INPUT_METHOD_SINCE"],"enum":["Event","Request"],"struct":["ZwpInputMethodManagerV2"]};
