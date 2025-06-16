8613c9ecc1ea9e01984f0d29be2533ae888c14c8
ids1024 <ids1024@users.noreply.github.com>
2025-06-16 20:09:40 +0000

    deploy: 6b2ba7d4fa2e868e4be688618081179c0e2517c0

---
+++ wayland_protocols_plasma/server_decoration/server/org_kde_kwin_server_decoration/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["EVT_MODE_OPCODE","EVT_MODE_SINCE","REQ_RELEASE_OPCODE","REQ_RELEASE_SINCE","REQ_REQUEST_MODE_OPCODE","REQ_REQUEST_MODE_SINCE"],"enum":["Event","Mode","Request"],"struct":["OrgKdeKwinServerDecoration"]};
